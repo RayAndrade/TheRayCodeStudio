ce49bfb6f79dd2cf1cfae3f562846cc85458cad7
Ray Andrade <andrade.ray@gmail.com>
2026-07-06 16:02:36 -0700

    *

---
+++ SQLitePython/database.py
@@ -0,0 +1,99 @@
+import sqlite3
+from PySide6.QtWidgets import *
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SQLite Database")
+        self.setGeometry(100, 100, 500, 100)
+
+        # create QLabel
+        name_label = QLabel("Name")
+        profession_label = QLabel("Profession")
+        address_label = QLabel("Address")
+        age_label = QLabel("Age")
+
+        # create QLine Edit
+        self.name_line_edit = QLineEdit()
+        self.profession_line_edit = QLineEdit()
+        self.address_line_edit = QLineEdit()
+        self.age_line_edit = QLineEdit()
+
+        # QPushButtons for adding and updating
+        button_add_data = QPushButton("Add new row")
+        button_add_data.clicked.connect(self.add_data)
+
+        button_update_data = QPushButton("Update selected row")
+        button_update_data.clicked.connect(self.update_data) # generated error
+
+        # name QLabel and Name QLineEdit form horizontal layout
+        h_layout1 = QHBoxLayout()
+        h_layout1.addWidget(name_label)
+        h_layout1.addWidget(self.name_line_edit)
+
+        # profession  QLabel and Name QLineEdit form horizontal layout
+        h_layout2 = QHBoxLayout()
+        h_layout2.addWidget(profession_label)
+        h_layout2.addWidget(self.profession_line_edit)
+
+        # address  QLabel and Name QLineEdit form horizontal layout
+        h_layout3 = QHBoxLayout()
+        h_layout3.addWidget(address_label)
+        h_layout3.addWidget(self.address_line_edit)
+
+        # age  QLabel and Name QLineEdit form horizontal layout
+        h_layout4 = QHBoxLayout()
+        h_layout4.addWidget(age_label)
+        h_layout4.addWidget(self.age_line_edit)
+
+        # QPushButtons   QLabel and Name QLineEdit form horizontal layout 6:42
+        h_layout5 = QHBoxLayout()
+        h_layout5.addWidget(button_add_data)
+        h_layout5.addWidget(button_update_data)
+
+        # GroupLabels, edits and push buttons  6:21
+        add_form = QGroupBox("Add new employee")
+
+        # Lay out items in group vertically
+        form_layout = QVBoxLayout()
+        form_layout.addLayout(h_layout1)
+        form_layout.addLayout(h_layout2)
+        form_layout.addLayout(h_layout3)
+        form_layout.addLayout(h_layout4)
+        form_layout.addLayout(h_layout5)
+        add_form.setLayout(form_layout)
+
+        # Create QTable  8:04
+        self.table = QTableWidget(self)
+        self.table.setMaximumWidth(800)
+
+        self.table.setColumnCount(4)
+        
+
+        # Display all elements vertically 7:28
+        layout = QVBoxLayout()
+        layout.addWidget(add_form)
+        self.setLayout(layout)
+
+    def create_connection(self):
+    # create SQLite database
+        self.connection = sqlite3.connect("employees.db")
+        return self.connection
+
+    def insert_data(self):
+        pass
+
+    def add_data(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def call_data(self):
+        pass
+        
+    def update_data(self):
+        pass
+        
+    def delete_data(self):
+        pass
